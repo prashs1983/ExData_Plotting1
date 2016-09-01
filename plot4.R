8d06613ee77a3d91d8caa8ea7a704b4da001cd04
prashs1983 <psharma_ece@yahoo.com>
2016-09-01 02:49:31 -0700

    initial

--- plot4.R
+++ plot4.R
@@ -4,6 +4,7 @@ plot4 <- function(){
   consumption_two_dates <- consumption[ !is.na(consumption$Date) & consumption$Date != "?" & consumption$Date != "" & (dmy(consumption$Date)==ymd("2007-02-01") | dmy(consumption$Date)==ymd("2007-02-02")),]
   
   png("plot4.png")
+  par(mfrow = c(2, 2))
   plot(dmy_hms(paste(consumption_two_dates$Date,consumption_two_dates$Time)),consumption_two_dates$Global_active_power, type = "l", xlab="", ylab="Global Active Power (killowatts)")
   plot(dmy_hms(paste(consumption_two_dates$Date,consumption_two_dates$Time)),consumption_two_dates$Voltage, type = "l", xlab="datetime", ylab="Voltage")
   
